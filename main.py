674f2e3521b785bce98de1edbadbf61afd3e2caa
Yu <86376823+yuyu786@users.noreply.github.com>
2024-01-11 16:25:04 +0630

    Update main.py

--- main.py
+++ main.py
@@ -121,5 +121,5 @@ if __name__ == "__main__":
     else:
         LOGGER.info("🔶Not Setting Up Bot Commands")
     LOGGER.info(f'✅@{telethob_bot.username} Started Successfully!✅')
-    LOGGER.info(f"⚡Bot By Sahil Nolia⚡")
+    LOGGER.info(f"⚡Bot By Bridi⚡")
     Telegram.TELETHON_CLIENT.run_until_disconnected()
